bfce931c97e758875d44f04e604bf1e87def2b96
Jim Fisher <jameshfisher@gmail.com>
2020-11-10 11:37:33 +0100

    more accurate xml feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,8 @@
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-
 const markdownIt = require("markdown-it");
 
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("assets");
-
-  eleventyConfig.addPlugin(pluginRss);
   
   const markdownItInstance = markdownIt({
     html: true,

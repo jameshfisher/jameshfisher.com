860db4d814af7f4a3b4add75045e299917d2f091
Jim Fisher <jameshfisher@gmail.com>
2020-11-10 15:26:01 +0100

    blogroll opml

---
+++ blogrollXml.11ty.js
@@ -0,0 +1,20 @@
+const blogroll = require('./blogroll.js');
+
+exports.data = {
+  permalink: "blogroll.xml"
+};
+
+exports.render = function(data) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+  <opml version="1.0">
+      <head>
+          <title>Jim's blogroll</title>
+      </head>
+      <body>
+        ${ blogroll
+          .filter(blog => blog.xmlUrl)
+          .map(blog => `<outline type="rss" xmlUrl="${ blog.xmlUrl }" htmlUrl="${ blog.htmlUrl }" title="${ blog.title }"/>`)
+          .join('\n') }
+      </body>
+  </opml>`;
+};

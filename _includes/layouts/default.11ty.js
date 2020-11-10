590670303924e5d99e87ebb834ceed79ce32b4b5
Jim Fisher <jameshfisher@gmail.com>
2020-11-10 15:00:55 +0100

    navbar module

--- _includes/layouts/default.11ty.js
+++ _includes/layouts/default.11ty.js
@@ -4,19 +4,7 @@ const striptags = require("striptags");
 
 const markdownItRenderer = new markdownIt();
 
-  // FIXME import/require this
-const navbarHtml = `<style>
-.navbar { display: flex; margin: 1em 0; }
-.navbar .navbar-item { flex-grow: 1; text-align: center; }
-</style>
-<div class="navbar">
-  <div class="navbar-item"><a href="/">Jim Fisher</a></div>
-  <div class="navbar-item"><a href="/speaking">Speaking</a></div>
-  <div class="navbar-item"><a href="/blogroll">Blogroll</a></div>
-  <div class="navbar-item"><a href="https://vidr.io">Vidrio</a></div>
-  <div class="navbar-item"><a href="https://jameshfisher.com/feed.xml">RSS</a></div>
-</div>
-`;
+const navbarHtml = require('../../navbar.js');
 
 const dataPeople = require('../../_data/people.js');
 
---
+++ navbar.js
@@ -0,0 +1,12 @@
+module.exports = `<style>
+.navbar { display: flex; margin: 1em 0; }
+.navbar .navbar-item { flex-grow: 1; text-align: center; }
+</style>
+<div class="navbar">
+  <div class="navbar-item"><a href="/">Jim Fisher</a></div>
+  <div class="navbar-item"><a href="/speaking">Speaking</a></div>
+  <div class="navbar-item"><a href="/blogroll">Blogroll</a></div>
+  <div class="navbar-item"><a href="https://vidr.io">Vidrio</a></div>
+  <div class="navbar-item"><a href="https://jameshfisher.com/feed.xml">RSS</a></div>
+</div>
+`;

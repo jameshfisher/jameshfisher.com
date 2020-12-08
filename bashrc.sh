492dba533f35a96fddadce96cc3045f328a7eab3
Jim Fisher <jameshfisher@gmail.com>
2020-12-08 20:16:56 +0000

    Allow working from Windows

--- bashrc.sh
+++ bashrc.sh
@@ -3,7 +3,7 @@
 # source this file from your ~/.bashrc
 
 function blogpost {
-  printf -- "---\ntitle: \"$1\"\ntags: []\n---\n\n" > "/Users/jim/dev/jameshfisher/jameshfisher.com/_posts/$(date '+%Y-%m-%d')-$(echo $1 | tr '[:upper:] ' '[:lower:]-' | tr -cd "[:alnum:]-").md"
+  printf -- "---\ntitle: \"$1\"\ntags: []\n---\n\n" > "${HOME}/dev/jameshfisher/jameshfisher.com/_posts/$(date '+%Y-%m-%d')-$(echo $1 | tr '[:upper:] ' '[:lower:]-' | tr -cd "[:alnum:]-").md"
 }
 
 alias blog="cd ~/dev/jameshfisher/jameshfisher.com && code -n . && npm run serve"

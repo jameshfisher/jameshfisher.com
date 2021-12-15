9c8c472d17c3732faaf441bc16838c322ae2d67f
Jim Fisher <jameshfisher@gmail.com>
2021-12-15 15:35:57 +0000

    cv

--- navbar.js
+++ navbar.js
@@ -5,8 +5,8 @@ module.exports = `<style>
 <div class="navbar">
   <div class="navbar-item"><a href="/">Jim Fisher</a></div>
   <div class="navbar-item"><a href="/speaking">Speaking</a></div>
+  <div class="navbar-item"><a href="/cv">CV</a></div>
   <div class="navbar-item"><a href="/blogroll">Blogroll</a></div>
-  <div class="navbar-item"><a href="https://vidr.io">Vidrio</a></div>
   <div class="navbar-item"><a href="https://jameshfisher.com/feed.xml">RSS</a></div>
 </div>
 `;

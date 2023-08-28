05c1f0c9a7112d17b280ff900b16f8a2ca899286
Jim Fisher <jameshfisher@gmail.com>
2023-08-28 11:32:13 +0200

    rm old banners

--- _includes/layouts/default.11ty.js
+++ _includes/layouts/default.11ty.js
@@ -25,64 +25,6 @@ exports.render = function (data) {
       }
       ${data.content}
       <div class="noprint">
-        <script>
-          window.discountCodes = {
-            findDiscountCode: () => {
-              console.log("Searching, beep boop ... 🔎");
-              setTimeout(() => {
-                console.error("Error: found codes at /assets/discounts.json, but insufficient power to decrypt!! 😱");
-              }, 2000);
-            }
-          };
-          console.log("window.discountCodes initialized! 🎉");
-        </script>
-        <a href="https://busy-beavers.tigyog.app/" target="_blank">
-          <div class="course">
-            <p>
-              What can computers do?
-              What are the limits of mathematics?
-              And just how busy can a busy beaver be?
-              This year, I’m writing <strong>Busy Beavers</strong>,
-              a unique interactive book on computability theory.
-              You and I will take a practical and modern approach to answering these questions — or at least learning why some questions are unanswerable!
-            </p>
-            <p>
-              It’s only $19, and you can get <strong>50% off</strong> if you find the discount code ... <span class="secret">Not quite. Hackers use the console!</span>
-            </p>
-          </div>
-        </a>
-        <a href="https://everyday-data-science.tigyog.app/" target="_blank">
-          <div class="course">
-            <p>
-              After months of secret toil, 
-              I and Andrew Carr released <strong>Everyday Data Science</strong>,
-              a unique interactive online course!
-              You’ll make the perfect glass of lemonade using Thompson sampling. 
-              You’ll lose weight with differential equations. 
-              And you might just qualify for the Olympics with a bit of statistics!
-            </p>
-            <p>
-              It’s $29, but you can get <strong>50% off</strong> if you find the discount code ... <span class="secret">Not quite. Hackers use the console!</span>
-            </p>
-          </div>
-        </a>
-        <style>
-          .course {
-            background: #f8f5f1;
-            color: #2c2621;
-            border-radius: 0.5em;
-            margin-top: 1em;
-            padding: 0.5em 1em;
-            transition: background 0.2s linear;
-          }
-          .course:hover {
-            background: #f5f0eb;
-          }
-          .secret {
-            color: black;
-            background-color: black;
-          }
-        </style>
         <h3>More by Jim</h3>
         <p class="posts">
           <ul>

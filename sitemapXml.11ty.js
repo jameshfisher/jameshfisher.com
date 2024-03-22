2f851efcdcfc306c51e89f722caa7c0c40bf3ffc
Jim Fisher <jameshfisher@gmail.com>
2024-03-22 13:53:33 +0000

    define an ordering in sitemap

--- sitemapXml.11ty.js
+++ sitemapXml.11ty.js
@@ -3,13 +3,16 @@ exports.data = {
 };
 
 exports.render = function (data) {
+  const allPagesByUrl = [...data.collections.all];
+  allPagesByUrl.sort((p1, p2) => p1.url.localeCompare(p2.url));
+
   return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset 
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
-  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 
-  http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" 
+<urlset
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+  http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
   xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${data.collections.all
+  ${allPagesByUrl
     .map(
       (page) => `<url>
   <loc>https://jameshfisher.com${page.url}</loc>

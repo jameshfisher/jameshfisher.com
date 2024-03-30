b5fa07b3e9d35fea6eb44c1e0d4a635a7b4f95c7
Jim Fisher <jameshfisher@gmail.com>
2024-03-30 16:15:07 +0000

    use titles in examples

--- scripts/addSummaries.ts
+++ scripts/addSummaries.ts
@@ -7,42 +7,68 @@ const anthropic = new Anthropic({
 });
 
 const examples = [
-  `A method for calculating a bounding circle around a head, using facial landmarks from BlazeFace. Plus a live demo that you can run on your own face.`,
-  `\`const\` is a type qualifier in C that makes a variable unassignable, except during initialization.`,
-  `A C function to determine if one string is a permutation of another, using a character distribution representation for optimal time and space complexity.`,
-  `To call a program from C, use \`fork\` then \`execve\`. There is no more direct way!`,
-  `An efficient way to represent and sample from a discrete probability distribution, using a balanced binary tree data structure called the "urn".`,
-  `The MediaStream Recording API converts a MediaStream to a Blob of compressed video and audio. A demo where you can record a 5-second clip.`,
-  `A screencast about \`go tool trace\`, a profiling tool for Go applications.`,
-  `How to apply a video filter to a webcam stream using the AVFoundation and CoreImage frameworks in Swift.`,
-  `\`tcpdump\` captures and displays network traffic. An example inspecting DNS requests and responses.`,
-  `Generating RSA keys, extracting the public key, encrypting with the public key, and decrypting with the private key.`,
-  `A green screen implementation in the browser using WebGL and chroma key. Includes a live demo.`,
-  `Computing a pointer to unowned memory invokes undefined behavior, even without dereferencing!`,
-  `Cookies are client-side storage that get sent with every HTTP request. A cookie is scoped to a domain suffix, a path prefix, and a time range. The API is old and weird.`,
-  `Installing Electron, creating an HTML web page, and writing the main entry point script to load the page.`,
+  {
+    title: "Head tracking with BlazeFace",
+    summary: `Calculating a bounding circle around a head, using facial landmarks from BlazeFace. Includes a demo that you can run on your own face.`,
+  },
+  {
+    title: "What does `const` mean in C?",
+    summary: `\`const\` is a type qualifier in C that makes a variable unassignable, except during initialization.`,
+  },
+  {
+    title: "Determine whether one string is a permutation of the other",
+    summary: `A C function to identify a permutation, using a character distribution representation for optimal time and space complexity.`,
+  },
+  {
+    title: "How do I call a program from C?",
+    summary: `To call a program from C, use \`fork\` then \`execve\`. There is no more direct way!`,
+  },
+  {
+    title: "ICFP: Ode on a Random Urn",
+    summary: `Representing and sampling from a discrete probability distribution, using a balanced binary tree data structure called the "urn".`,
+  },
+  {
+    title: "MediaRecorder hello world",
+    summary: `The MediaStream Recording API converts a MediaStream to a Blob of compressed video and audio. Includes a demo where you can record a 5-second clip.`,
+  },
+  {
+    title: "How to run `go tool trace`",
+    summary: `A screencast about \`go tool trace\`, a profiling tool for Go applications.`,
+  },
+  {
+    title: "What is `tcpdump`?",
+    summary: `\`tcpdump\` captures and displays network traffic. An example inspecting DNS requests and responses.`,
+  },
+  {
+    title: "Does C allow pointer arithmetic?",
+    summary: `Computing a pointer to unowned memory invokes undefined behavior, even without dereferencing!`,
+  },
 ];
 
 async function fileToSummary(fileContent: string): Promise<string> {
+  const systemPrompt =
+    [
+      `You are given an excerpt of a post from jameshfisher.com, Jim Fisher's blog.`,
+      `You respond with a TL;DR of 1 or 2 sentences.`,
+      `The TL;DR will be added to the post front-matter.`,
+      `The TL;DR is shown beneath links to the post.`,
+      `You are Jim Fisher, and write using the style and vocabulary of the examples and the post.`,
+      `Paraphrase the content directly.`,
+      `Never mention 'the post'.`,
+      `Be extremely concise, even using sentence fragments.`,
+      `Do not duplicate info from the title.`,
+      `Only include information from the post.`,
+      `Use Markdown for formatting.`,
+      `Excellent examples of TL;DRs from other posts:`,
+    ].join(" ") +
+    `\n\n` +
+    examples
+      .map(({ title, summary }) => `Title: ${title}\nTL;DR: ${summary}`)
+      .join("\n\n");
+  console.log({ systemPrompt });
   const message = await anthropic.messages.create({
     max_tokens: 1024,
-    system:
-      [
-        `You are given an excerpt of a post from jameshfisher.com, Jim Fisher's blog.`,
-        `You respond with a TL;DR of 1 or 2 sentences.`,
-        `The TL;DR will be added to the post front-matter.`,
-        `The TL;DR is shown beneath links to the post.`,
-        `You are Jim Fisher, and write using the style and vocabulary of the examples and the post.`,
-        `Paraphrase the content directly.`,
-        `Never mention 'the post'.`,
-        `Be extremely concise, even using sentence fragments.`,
-        `Do not duplicate info from the title.`,
-        `Only include information from the post.`,
-        `Use Markdown for formatting.`,
-        `Excellent examples of TL;DRs from other posts:`,
-      ].join(" ") +
-      `\n` +
-      examples.map((summary) => `- TL;DR: ${summary}`).join("`n"),
+    system: systemPrompt,
     messages: [
       { role: "user", content: fileContent.slice(0, 16384) },
       { role: "assistant", content: `TL;DR:` },

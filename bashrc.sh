6b6bb14e86382e2c4c3cd135478b9924c33e5aaf
Jim Fisher <jameshfisher@gmail.com>
2024-03-31 16:15:48 +0100

    update to new structure

--- bashrc.sh
+++ bashrc.sh
@@ -3,7 +3,11 @@
 # source this file from your ~/.bashrc
 
 function blogpost {
-  printf -- "---\ntitle: \"$1\"\ntags: []\n---\n\n" > "${HOME}/dev/jameshfisher/jameshfisher.com/_posts/$(date '+%Y-%m-%d')-$(echo $1 | tr '[:upper:] ' '[:lower:]-' | tr -cd "[:alnum:]-").md"
+  local blogpost_slug=$(echo $1 | tr '[:upper:] ' '[:lower:]-' | tr -cd "[:alnum:]-")
+  local blogpost_date=$(date '+%Y-%m-%d')
+  local blogpost_dir="${HOME}/dev/jameshfisher/jameshfisher.com/_posts/${blogpost_date}-${blogpost_slug}"
+  mkdir -p $blogpost_dir
+  printf -- "---\ntitle: \"$1\"\ntags: []\n---\n\n" > "${blogpost_dir}/index.md"
 }
 
 alias blog="cd ~/dev/jameshfisher/jameshfisher.com && code -n . && npm run serve"

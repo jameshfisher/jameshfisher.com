9251eee03070337e332ccf53c5058daf7f7cbf0d
Jim Fisher <jameshfisher@gmail.com>
2024-03-31 11:57:51 +0100

    a bit less magic

--- _posts/_posts.11tydata.js
+++ _posts/_posts.11tydata.js
@@ -1,8 +1,15 @@
 // https://www.11ty.dev/docs/data-template-dir/
 
+// format YYYY/MM/DD, including leading zeros
+function formatDate(date) {
+  return date.toISOString().slice(0, 10).replace(/-/g, "/");
+}
+
 export default {
   filetype: "blogpost",
-  permalink: "{{ page.date | date: '%Y/%m/%d' }}/{{page.fileSlug}}/index.html",
+  permalink: (data) =>
+    `/${formatDate(data.page.date)}/${data.page.fileSlug}/index.html`,
+
   layout: "layouts/default",
   justification: "I felt like it.",
   author: "jim",
